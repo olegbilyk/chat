a4a5f396553a204d7b46087da34d30fdee1c9f9a
olegbilyk <oleg.bilik1994@gmail.com>
2017-09-24 16:40:28 +0300

    fix responsive

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,6 @@
 const webpack = require('webpack')
 const path = require('path')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
 const develop = process.env.NODE_ENV !== 'production'
 
 module.exports = {
